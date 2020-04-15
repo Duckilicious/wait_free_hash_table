3735406e64508c14d41ecc23f85431c0b0534ac2
Tal Gelbard <talgelbard1@gmail.com>
2020-04-16 00:08:30 +0300

    Wrote prefix and tighten the code
    Also found a bug

--- hashmap.h
+++ hashmap.h
@@ -12,12 +12,10 @@
 #define BIGWORD_SIZE (16)
 #define POW(exp) ((unsigned)2 << (unsigned)(exp))
 
-
 #include <iostream>
 #include <pthread.h>
 #include <atomic>
 #include <cassert>
-
 
 template<typename Key, typename Value>
 class hashmap {
@@ -27,19 +25,27 @@ class hashmap {
         Key key;
         Value value;
     };
+
     enum Op_type {
         INS, DEL
     };
+
     enum Status_type {
         TRUE, FALSE, FAIL
     };
-    struct Operation {
+
+    class Operation {
+    public:
         Op_type type;
         Key key;
         Value value;
         int seqnum;
         size_t hash;
+
+        Operation(Op_type t, Key k, Value v, int seq, size_t h) :
+            type(t), key(k), value(v), seqnum(seq), hash(h) {};
     };
+
     struct Result {
         Status_type status;
         int seqnum;
@@ -48,21 +54,27 @@ class hashmap {
     class BigWord {
         uint8_t Data[BIGWORD_SIZE];
         BigWord() : Data() {}
+
         BigWord(BigWord const& b) : Data() {
             for (int i = 0; i < BIGWORD_SIZE; ++i) {
                 this->Data[i] = b.Data[i];
             }
         }
+
         BigWord operator=(BigWord b) = delete;
+
         ~BigWord() = default;
-        void Flip(unsigned int id) {
-            this->Data[(id/8)] |= (int8_t)(1 << (id % 8)); // I came up with this myself, I think I deserve a medal
+
+        void setBit(unsigned int id) {
+            Data[id/8] |= (int8_t)(1 << (id % 8));
         }
-        bool bitOn(unsigned int id) const {
-            auto mask = (int8_t)(1 << (id % 8));
-            auto temp = this->Data[(id/8)];
-            temp &= mask;
-            return temp == mask;
+
+        void clearBit(unsigned int id){
+            Data[id/8] &= (uint8_t)~(1 << (id % 8));
+        }
+
+        bool testBit(unsigned int id) const {
+            return ((Data[id/8] & (1 << (id % 8))) != 0);
         }
     };
 
@@ -85,6 +97,7 @@ class hashmap {
             this->applied(b.applied);
         }
         BState operator=(BState b) = delete; // we can implement assignment constructor if we will need to, but it cannot be the default so for now so it's blocked
+
         bool insertItem(Triple const *t) {
             for (Triple* &item : items) {
                 if (!item) {
@@ -94,70 +107,80 @@ class hashmap {
             }
             return false; // bucket is full
         }
-        int BucketFull() const {
+
+        int BucketFull() const { //Returns the free entry if it exists, else -1
             for (int i = 0; i < BUCKET_SIZE; i ++) {
                 if (this->items[i] == nullptr)
                     return i;
             }
             return FULL_BUCKET;
         }
+
         ~BState() = default;
     };
 
     class Bucket {
         int prefix;
-        int depth;
+        size_t depth;
         volatile std::atomic<BState*> state; //TODO: volatile means un-cacheable this is the CAS API demands, but maybe we can find a better CAS that doesn't require this since this is more exspensive
         BigWord toggle; // 128 bit array
+
     public:
         Bucket() : prefix(), depth(), state(std::atomic<int>(new BState())), toggle() {}
+
         Bucket(const Bucket &b) : prefix(b.prefix), depth(b.depth),
             state(std::atomic<BState*>(new BState(*(b->state->load(std::memory_order_relaxed))))),
             toggle(b.toggle) { } // copy constructor
+
         Bucket operator=(Bucket b) = delete; // we can implement assignment constructor if we will need to, but it cannot be the default so for now so it's blocked
+
         ~Bucket() = default;
     };
 
     class DState {
-        int depth;
+        size_t depth;
         Bucket *dir[];  // allocated array with size 2^depth
+
+        size_t getDepth(){
+            return depth;
+        }
     public:
-        explicit DState(int depth) : depth(depth){
+        explicit DState(size_t depth) : depth(depth){
             assert(0 < depth && depth < 20);
             dir = new Bucket*[POW(depth)]();
             // instead of including <cmath> used quick power trick "<<"
             // auto initiated with nullptr
         }
+
         DState(const DState &b) = delete; // we can implement copy constructor if we will need to, but it cannot be the default so for now so it's blocked
+
         DState operator=(DState b) = delete; // we can implement assignment constructor if we will need to, but it cannot be the default so for now so it's blocked
+
         void EnlargeDir() {
             Bucket *new_dir[] = new Bucket*[POW(depth + 1)]();
-            for (int i = 0; i < (unsigned)0x1 << (unsigned)(depth); ++i) {
+            unsigned int dir_size = (unsigned)0x1 << (unsigned)(depth);
+
+            for (int i = 0; i < dir_size; ++i) {
                 new_dir[(i << 1) + 0] = dir[i];
                 new_dir[(i << 1) + 1] = dir[i];
             }
+
             delete [] dir;
-            dir = new_dir; // pointer assignment
+            dir = new_dir; // NOTE: This is a bug the pointer to the array will contain garbage when you exit this function
             depth++;
         }
+
         ~DState() {
             delete[] dir;
         }
     };
 
-
-    // Global variables of the class:
-
+    /*** Global variables of the class goes below: ***/
     volatile std::atomic<DState*> ht;
     Operation *help[NUMBER_OF_THREADS]; // array size n
     int opSeqnum[NUMBER_OF_THREADS]; // This will be an array of size N and each thread will only access to its memory space
 
-
-    // Inner function section:
-
-    /*** We can make this function a constant with a flag later on ***/
-
-    // Also returns the last available space in the array
+    /*** Inner function section goes below: ***/
 
     int existInBucket(Triple* const items[], Triple const& c) {
         for(int i = 0; i < BUCKET_SIZE; i++) {
@@ -167,30 +190,19 @@ class hashmap {
         return NOT_FOUND;
     }
 
-    void Flip(BigWord &bitArray, unsigned int id) {
-        bitArray.Data[(id/8)] |= (int8_t)(1 << (id % 8)); // I came up with this myself, I think I deserve a medal
-    }
-
-    bool bitOn(BigWord bitarray, unsigned int id){
-        auto mask = (int8_t)(1 << (id % 8));
-        auto temp = bitarray.Data[(id/8)];
-        temp &= mask;
-        return temp == mask;
-    }
-
     void ApplyWFOp(Bucket &b, unsigned int id) {
         std::atomic<BState*> newBState_atomic;
-        unsigned long oldToggle;
+        BigWord oldToggle;
         BState *oldBState, *newBState;
 
-        Flip(b.toggle, id); // mark as worked on by thread id
+        b.toggle.setBit(id); // mark as worked on by thread id
 
         for(int i = 0; i < 2; i++) {
             oldBState = b.state->load(std::memory_order_relaxed); // this is the most efficient access, it is suppose to work since the algo already covers all the cases
             newBState = new BState(oldBState);
             oldToggle = b.toggle;
 
-            for(unsigned int j = 0; (j < NUMBER_OF_THREADS) &&  (bitOn(oldToggle, j) != bitOn(oldBState->applied, j)); j++) {
+            for(unsigned int j = 0; (j < NUMBER_OF_THREADS) && (oldToggle.testBit(j) != oldBState->applied.testBit(j)); j++) {
                 if(newBState->results[j].seqnum < help[j]->seqnum) {
                     newBState->results[j].status = ExecOnBucket(newBState, *help[j]);
                     if(newBState->results[j].status != FAIL)
@@ -208,7 +220,7 @@ class hashmap {
         }
     }
 
-    Status_type ExecOnBucket(BState *b, Operation const &op) {
+    Status_type ExecOnBucket(BState *b, Operation const &op) { //TODO this function will now not work correctly
         int updateID = 0;
         int freeID = 0;
         Triple *temp;
@@ -277,7 +289,7 @@ class hashmap {
         for (Bucket const *b : blist) {
             if (b->depth > d.depth) d.EnlargeDir();
             // TODO: "entries" part is really heavy and should be shorten: (I need help with that)
-            for (int e = 0; e < POW(d.depth); ++e) {
+            for (size_t e = 0; e < POW(d.depth); ++e) {
                 if (Prefix(e, b->depth) == b->prefix)
                     d.dir[e] = b;
             }
@@ -310,7 +322,7 @@ class hashmap {
             DState const *newD = new DState(*oldD);
             for (int j = 0; j < NUMBER_OF_THREADS; ++j) {
                 if (help[j]) {
-                    Bucket *b = newD->dir[Prefix(help[j].key, newD->depth)];
+                    Bucket *b = newD->dir[Prefix(help[j]->hash, newD->depth)];
                     BState &bs = *(b->state->load(std::memory_order_relaxed));
                     if (bs.BucketFull() && bs.results[j].seqnum < help[j]->seqnum) {
                         ApplyPendingResize(newD, *b);
@@ -324,13 +336,20 @@ class hashmap {
         }
     }
 
-    int Prefix(Key, int);
+    size_t Prefix(size_t hash, size_t depth) {
+        assert(depth);
+        int64_t mask = (1 << depth) - 1; //This will always result in all the first depth bits on
+        int64_t prefix = (int64_t)hash & mask;
+        return (size_t)prefix;
+    }
 
 
 public:
     hashmap() : ht(std::atomic<DState*>(new DState(1))), help(), opSeqnum() {};
     hashmap(hashmap&) = delete;
     hashmap operator=(hashmap) = delete;
+
+
     ~hashmap() {
         DState* d = *(ht->load(std::memory_order_relaxed));
         // TODO: delete all the buckets and...
@@ -355,17 +374,18 @@ public:
 
     enum Status_type insert(Key const &key, Value const &value, unsigned int id) {
 
-        help[id] = {INS, key, value, ++opSeqnum[id]};
-        DState htl = *(ht->load(std::memory_order_relaxed));
         std::size_t hashed_key = std::hash<Key>(key); // TODO: fix hash
-        ApplyWFOp(htl.dir[hashed_key]);
+        help[id] = new Operation(INS, key, value, ++opSeqnum[id], hashed_key);
+        DState htl = (ht->load(std::memory_order_relaxed));
+        size_t hash_prefix =  Prefix(hashed_key, htl.getDepth());
 
+        ApplyWFOp(htl.dir[hashed_key]);
         htl = *(ht->load(std::memory_order_relaxed));
-        if(htl.dir[hashed_key].state->results[id].seqnum != opSeqnum[id])
+        if (htl.dir[hash_prefix].state->results[id].seqnum != opSeqnum[id])
             ResizeWF();
         htl = *(ht->load(std::memory_order_relaxed));
-        return htl.dir[hashed_key].state->results[id].status;
-    };
+        return htl.dir[hash_prefix].state->results[id].status;
+    }
 
 
 
